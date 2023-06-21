358fdd4fccb5105006f2af3ee54c00886e5c26b1
sandman-code <cade3217@gmail.com>
2023-06-21 11:54:32 -0400

    changed dockerfile to get rid of secrets

--- server/src/app.py
+++ server/src/app.py
@@ -10,7 +10,7 @@ from numpy.linalg import norm
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
-'''
+"""
 Use cases:
 
 User enters word
@@ -24,8 +24,7 @@ User enters word
     - Fetch more like word
 
     - Return word score and direction words
-'''
-
+"""
 
 
 @app.route("/")
@@ -33,47 +32,42 @@ def hello_world():
     return "<h1>Lagom Health Check</h1>"
 
 
-@app.route("/guess/<int:game>/<word>")
+@app.route("/game/<int:game>/<word>")
 def guess(game, word):
+    word_of_the_day = "miyuki"
 
-    word_of_the_day = "apple"
+    if word == word_of_the_day:
+        return {"guess": word.lower(), "score": 1, "isWinner": True}
 
-    if(word == word_of_the_day):
-        return {
-            "guess": word.lower(),
-            "score": 1,
-            "isWinner": True
-        }
+    PINECONE_API_KEY = os.environ["PINECONE_API_KEY"]
 
-    PINECONE_API_KEY = os.environ['PINECONE_API_KEY']
+    pinecone.init(api_key=PINECONE_API_KEY, environment="northamerica-northeast1-gcp")
 
-    pinecone.init(api_key=PINECONE_API_KEY,
-              environment="northamerica-northeast1-gcp")
-    
     index = pinecone.Index("lagom")
- 
+
     wotd = index.fetch([word_of_the_day])
 
-    wotd_values = wotd['vectors'][word_of_the_day]['values']
-    
-    guess = index.fetch([word.lower()])
-    
+    wotd_values = wotd["vectors"][word_of_the_day]["values"]
 
+    guess = index.fetch([word.lower()])
 
     try:
-        guess_values = guess['vectors'][word.lower()]['values']
+        guess_values = guess["vectors"][word.lower()]["values"]
     except:
-        res = make_response("Unable to find word. Currently Lagom is running on a small dataset. Improvements coming soon!", 500)
+        res = make_response(
+            "Unable to find word. Currently Lagom is running on a small dataset. Improvements coming soon!",
+            500,
+        )
         return res
 
     # magic fucking numbers
-    cos = (np.dot(wotd_values,guess_values)/(norm(wotd_values)*norm(guess_values)) - 0.5) * 2
-    
-    return {
-        "guess": word.lower(),
-        "score": round(cos, 2),
-        "isWinner": False
-    }
+    cos = (
+        np.dot(wotd_values, guess_values) / (norm(wotd_values) * norm(guess_values))
+        - 0.5
+    ) * 2
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT',8080)))
+    return {"guess": word.lower(), "score": round(cos, 2), "isWinner": False}
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
